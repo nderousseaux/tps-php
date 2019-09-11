9bfe3f9e4387f1b0fb9eb4020fe0912009c0cffe
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-09-11 17:29:47 +0200

    3 premiers points de l'exercice 1

---
+++ TP/TP1/bonjourGet.php
@@ -0,0 +1,11 @@
+<html lang="français">
+<head>
+    <title>HTML !</title>
+</head>
+<body>
+    <p>
+    <?php echo 'Bienvenue sur le module :' .$_GET['module']; ?>
+    </p>
+    <p>Bonjour à toi</p>
+</body>
+</html>
---
+++ TP/TP1/bonjourPost.php
@@ -0,0 +1,14 @@
+<html lang="français">
+<head>
+    <title>HTML !</title>
+
+</head>
+<body>
+
+
+    
+    
+    <?php echo 'Bonjour ' .$_POST['firstname']. ' ' .$_POST['lastname'] ?>
+    
+</body>
+</html>
