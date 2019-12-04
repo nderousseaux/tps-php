68ba7c0c0dff37169980989df615a9f3bb697013
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-12-04 11:05:49 +0100

    Demarrage JAC

---
+++ TP/JAC/d/database/migrations/2019_11_27_112602_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Users', function (Blueprint $table) {
+            $table->string('user',60);
+            $table->string('password',255);
+            $table->primary('user');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Users');
+    }
+}
